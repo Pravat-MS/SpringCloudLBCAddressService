601ed687d597a2cfb52cc3e6d234872456db33f0
saipravatkumar1996 <saipravatkumar1996@gmail.com>
2023-10-20 22:30:18 +0530

    Spring Cloud LBC Address Service

---
+++ src/main/java/com/pravat/ms/SpringCloudLbcAddressServiceApplication.java
@@ -0,0 +1,15 @@
+package com.pravat.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudLbcAddressServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLbcAddressServiceApplication.class, args);
+	}
+
+}
